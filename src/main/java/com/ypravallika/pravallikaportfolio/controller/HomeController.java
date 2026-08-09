56057ffabf70f02a62286a74e15e9ed84dc5dcbf
pravallikayeturu <ypravallika46@gmail.com>
2026-08-09 12:07:00 +0530

    Allow Vercel frontend in CORS

--- src/main/java/com/ypravallika/pravallikaportfolio/controller/HomeController.java
+++ src/main/java/com/ypravallika/pravallikaportfolio/controller/HomeController.java
@@ -10,7 +10,8 @@ import com.ypravallika.pravallikaportfolio.service.ContactService;
     "http://127.0.0.1:5501",
     "http://localhost:5501",
     "http://127.0.0.1:5500",
-    "http://localhost:5500"
+    "http://localhost:5500",
+    "https://pravallika-portfolio-three.vercel.app"
 })
 @RestController
 @RequestMapping("/api")
